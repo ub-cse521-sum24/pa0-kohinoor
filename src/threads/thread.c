7c852deb3d7cca1427bf2963c690424a9c6b260c
adarsh-257 <102864708+adarsh-257@users.noreply.github.com>
2024-06-07 02:56:30 +0000

    PA0 Fix

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -335,7 +335,7 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  /* Not yet implemented. */
+  thread_current()->priority=new_priority;
 }
 
 /* Returns the current thread's priority. */
